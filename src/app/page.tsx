3e2c0e8a8352b62a85ad3618e12488f445b756ee
Andrew <Andrew@thesocialdiner.com>
2025-04-22 00:18:46 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import {CollectionDisplay} from '@/components/CollectionDisplay';
 // import {SidebarProvider} from '@/components/ui/sidebar'; // Remove SidebarProvider
 import {Toaster} from '@/components/ui/toaster';
+import inventoryData from '@/data/master_inventory_colab.json'; // Import the JSON data
 
 export default function Home() {
   return (
     <>
-      <CollectionDisplay />
+      {/* Pass the imported data as a prop */}
+      <CollectionDisplay items={inventoryData} /> 
       <Toaster />
     </>
   );
 }
-
